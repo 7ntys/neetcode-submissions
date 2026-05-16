089c18a0f3477404e6b87e2d15c1464a59fa2c03
7ntys <61086353+7ntys@users.noreply.github.com>
2026-05-16 12:18:25 +0200

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.go
@@ -0,0 +1,16 @@
+func change(amount int, coins []int) int {
+    return dfs(amount, coins, 0, make(map[[2]int]int))
+}
+
+func dfs(a int, coins []int, i int, memo map[[2]int]int) int {
+	if a == 0 {return 1}
+	if a < 0 || i == len(coins) {return 0}
+
+	key := [2]int{a,i}
+	if v, ok := memo[key]; ok {return v}
+
+	// either take or not coins[i]
+	memo[key] += dfs(a - coins[i], coins, i, memo)
+	memo[key] += dfs(a, coins, i + 1, memo)
+	return memo[key]
+}
