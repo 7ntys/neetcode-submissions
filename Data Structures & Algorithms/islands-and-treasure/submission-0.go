f97081a536fb2b4277b36d561a36ff928b2185ee
7ntys <61086353+7ntys@users.noreply.github.com>
2026-05-19 10:27:34 +0200

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.go
@@ -0,0 +1,47 @@
+var positions = [][2]int{
+	{1,0},
+	{-1,0},
+	{0,1},
+	{0,-1},
+}
+
+func islandsAndTreasure(grid [][]int) {
+    q := []Point{}
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == 0 {
+				q = append(q, Point{i: i, j: j})
+			}
+		}
+	}
+
+	for len(q) > 0 {
+		size := len(q)
+
+		for i := 0; i < size; i++ {
+			p := q[i]
+
+			for _, pos := range positions {
+				x, y := p.i + pos[0], p.j + pos[1]
+
+				if x < 0 || x >= len(grid) {continue}
+				if y < 0 || y >= len(grid[x]) {continue}
+				if grid[x][y] == -1 {continue}
+
+				if grid[x][y] > grid[p.i][p.j] + 1 {
+					grid[x][y] = grid[p.i][p.j] + 1
+					q = append(q, Point{i: x, j: y})
+				}
+			}
+		}
+
+		q = q[size:]
+	}
+}
+
+
+type Point struct {
+	i int
+	j int
+}
